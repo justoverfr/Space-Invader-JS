6065b7ec47eb1e7195a2cdab12f46231695d43ab
esteban <esteban.dardillac@gmail.com>
2023-01-04 10:50:51 +0100

    missile

---
+++ assets/js/missile.js
@@ -0,0 +1,9 @@
+import { shipPosX, shipPosY } from "./ship";
+
+var posXmissile = shipPosX
+var posYmissile = shipPosY
+var domMissile = document.getElementsByClassName("laser")
+
+function tireMissile(){
+    var key = event.keyCode
+}
